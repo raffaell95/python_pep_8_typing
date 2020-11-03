c8db7583d0d2f38ccdbd7413e616274e959809ef
raffaell95 <raffaell95@live.com>
2020-11-02 22:59:13 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from fila_normal import FilaNormal
+from fila_prioritaria import FilaPrioritaria
+
+fila_teste2 = FilaPrioritaria()
+fila_teste2.atualiza_fila()
+fila_teste2.atualiza_fila()
+fila_teste2.atualiza_fila()
+print(fila_teste2.chama_cliente(10))
+print(fila_teste2.chama_cliente(1))
+print(fila_teste2.estatistica('02/11/2020', 198, 'detail'))
