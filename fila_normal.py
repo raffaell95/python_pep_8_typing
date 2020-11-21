9e058b63cd0422f149b06a36790f6f28de89b13c
raffaell95 <raffaell95@live.com>
2020-11-20 21:01:00 -0300

    melhorando a tipagem

--- fila_normal.py
+++ fila_normal.py
@@ -1,12 +1,13 @@
 from fila_base import FilaBase
+from constantes import CODIGO_NORMAIL
 
 
 class FilaNormal(FilaBase):
     def gera_senha_atual(self) -> None:
-        self.senha_atual = f'NM{self.codigo}'
+        self.senha_atual = f'{CODIGO_NORMAIL}{self.codigo}'
 
-        def chama_cliente(self, caixa: int) -> str:
-            cliente_atual = self.fila.pop(0)
-            self.clientes_atendidos.append(cliente_atual)
-            return f'Cliente atual: {cliente_atual}, ' \
-                   f'dirija-se ao caixa {caixa}'
+    def chama_cliente(self, caixa: int) -> str:
+        cliente_atual = self.fila.pop(0)
+        self.clientes_atendidos.append(cliente_atual)
+        return f'Cliente atual: {cliente_atual}, ' \
+            f'dirija-se ao caixa {caixa}'
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from fila_normal import FilaNormal
-from fila_prioritaria import FilaPrioritaria
+from fabrica_fila import FabricaFila
 
 
 # fila_teste = filanormal()
-# fila_teste.atualizafila()
-# fila_teste.atualizafila()
-# fila_teste.atualizafila()
-# fila_teste.atualizafila()
-# print(fila_teste.chamacliente(5))
-# print(fila_teste.chamacliente(10))
+# fila_teste.atualiza_fila()
+# fila_teste.atualiza_fila()
+# fila_teste.atualiza_fila()
+# fila_teste.atualiza_fila()
+# print(fila_teste.chama_cliente(5))
+# print(fila_teste.chama_cliente(10))
 
-fila_teste_2 = FilaPrioritaria()
-fila_teste_2.atualiza_fila()
-fila_teste_2.atualiza_fila()
-fila_teste_2.atualiza_fila()
-print(fila_teste_2.chama_cliente(10))
-print(fila_teste_2.estatistica('10/01/1993', 198, 'detail'))
+# fila_teste_2 = FilaPrioritaria()
+# fila_teste_2.atualiza_fila()
+# fila_teste_2.atualiza_fila()
+# fila_teste_2.atualiza_fila()
+# print(fila_teste_2.chama_cliente(10))
+# print(fila_teste_2.estatistica('10/01/1993', 198, 'detail'))
+
+teste_fabrica = FabricaFila.pega_fila('normal')
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+print(teste_fabrica.chama_cliente(10))
