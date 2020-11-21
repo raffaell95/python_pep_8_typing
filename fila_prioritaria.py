59fbe2a558a8835d183a62164aff1d3394d6797c
raffaell95 <raffaell95@live.com>
2020-11-20 21:51:38 -0300

    Injeção de dependencia

--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -2,6 +2,10 @@ from typing import Dict, List, Union
 
 from fila_base import FilaBase
 from constantes import CODIGO_PRIORITARIO
+from estatistica_detalhada import EstatisticaDetalhada
+from estatistica_resumida import EstatisticaResumida
+
+Classes = Union[EstatisticaResumida, EstatisticaDetalhada]
 
 
 class FilaPrioritaria(FilaBase):
@@ -14,15 +18,6 @@ class FilaPrioritaria(FilaBase):
                                        f'{cliente_atual} - Caixa {caixa}')
         return(f'CLiente atual: {cliente_atual}, dirija-se ao caixa: {caixa}')
 
-    def estatistica(self, dia: str, agencia: str, flag: str) -> dict:
-        estatistica: Dict[str, Union[List[str], str, int]] = {}
-        if flag != 'detail':
-            estatistica[f'{agencia} - {dia}'] = len(self.clientes_atendidos)
-        else:
-            estatistica['dia'] = dia
-            estatistica['agencia'] = agencia
-            estatistica['clientes atendidos'] = self.clientes_atendidos
-            estatistica['quantidade de clientes atendidos'] = (
-                len(self.clientes_atendidos)
-            )
-        return estatistica
+    def estatistica(self, retorna_estatistica: Classes) -> dict:
+
+        return retorna_estatistica.roda_estatistica(self.clientes_atendidos)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fabrica_fila import FabricaFila
-
+from estatistica_detalhada import EstatisticaDetalhada
+from estatistica_resumida import EstatisticaResumida
 
 # fila_teste = filanormal()
 # fila_teste.atualiza_fila()
@@ -16,8 +17,10 @@ from fabrica_fila import FabricaFila
 # print(fila_teste_2.chama_cliente(10))
 # print(fila_teste_2.estatistica('10/01/1993', 198, 'detail'))
 
-teste_fabrica = FabricaFila.pega_fila('normal')
+teste_fabrica = FabricaFila.pega_fila('prioritaria')
 teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
 print(teste_fabrica.chama_cliente(10))
+print(teste_fabrica.chama_cliente(10))
+print(teste_fabrica.estatistica(EstatisticaResumida('20/03/2020', 120)))
